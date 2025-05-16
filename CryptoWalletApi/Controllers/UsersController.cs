cb80609450dd970b08521171ddc9715e7ba30a97
Lucas-AVS <lucas.alencar31@gmail.com>
2025-05-15 22:38:24 -0300

    fix: now has a proper basic structure working (get/post)

--- CryptoWalletApi/Controllers/UsersController.cs
+++ CryptoWalletApi/Controllers/UsersController.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using CryptoWalletApi.Data;
 using CryptoWalletApi.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using MinimalAPIDemo.Models;
@@ -23,36 +25,76 @@ namespace CryptoWalletApi.Controllers
         }
 
         [HttpGet]
-        public JsonResult Get()
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status200OK)]
+        public IActionResult GetAll()
         {
-            var users = _context.Users.ToList();
-            return new JsonResult(Ok(users));
+            var response = new ApiResponse()
+            {
+                IsSuccess = true,
+                Result = _context.Users.ToList(),
+                StatusCode = HttpStatusCode.OK
+            };
+            return Ok(response);
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
+        public IActionResult Get(int id)
+        {
+            var user = _context.Users.FirstOrDefault(u => u.Id == id);
+            var response = new ApiResponse();
+            if (user is null)
+            {
+                response.IsSuccess = false;
+                response.StatusCode = HttpStatusCode.NotFound;
+                response.ErrorMessages = new List<string> { $"User {id} not found" };
+                return NotFound(response);
+            }
+
+            response.IsSuccess = true;
+            response.Result = user;
+            response.StatusCode = HttpStatusCode.OK;
+            return Ok(response);
         }
 
 
-        // [HttpGet("{id}")]
-        // public IActionResult GetById(int id)
-        // {
-        //     return Ok($"Hello from User controller with id {id}");
-        // }
-
         [HttpPost]
-        public JsonResult Create([FromBody] string name)
-
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status201Created)]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status400BadRequest)]
+        public IActionResult Create([FromBody] string name, string email, string password)
         {
+            var response = new ApiResponse()
+            {
+                IsSuccess = false,
+                StatusCode = HttpStatusCode.BadRequest
+            };
+
+            if (name is null || email is null || password is null)
+            {
+                return BadRequest(response);
+            }
+
+            var HighId = _context.Users.Max(u => (int?)u.Id);
+
             var user = new User
             {
-                Id = 1,
+                Id = HighId == null ? 1 : HighId.Value + 1,
                 Name = name,
-                Email = "aa@email.com",
+                Email = email,
                 Wallets = null,
-                PasswordHash = "aaa"
+                PasswordHash = password // will be changed
 
             };
             _context.Users.Add(user);
+
+            response.IsSuccess = true;
+            response.Result = user;
+            response.StatusCode = HttpStatusCode.Created;
+
             _context.SaveChanges();
 
-            return new JsonResult(CreatedAtAction(nameof(User), new { name = user.Name }, user));
+            return Created($"/api/friends/{user.Name}", response);
         }
     }
 }
