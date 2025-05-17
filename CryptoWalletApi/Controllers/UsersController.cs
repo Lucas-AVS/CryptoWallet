d382eb3b535d1791086204bca0d42cc79f3a7e82
Lucas-AVS <lucas.alencar31@gmail.com>
2025-05-16 22:00:11 -0300

    feat: add user DTOs to user controller

--- CryptoWalletApi/Controllers/UsersController.cs
+++ CryptoWalletApi/Controllers/UsersController.cs
@@ -1,15 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
 using System.Net;
-using System.Threading.Tasks;
+using AutoMapper;
 using CryptoWalletApi.Data;
+using CryptoWalletApi.DTO;
 using CryptoWalletApi.Models;
-using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using MinimalAPIDemo.Models;
+
+
+
 
 namespace CryptoWalletApi.Controllers
 {
@@ -18,20 +15,26 @@ namespace CryptoWalletApi.Controllers
     public class UsersController : ControllerBase
     {
         private readonly ApiContext _context;
+        private readonly IMapper _mapper;
 
-        public UsersController(ApiContext context)
+        public UsersController(ApiContext context, IMapper mapper)
         {
             _context = context;
+            _mapper = mapper;
         }
+
+
 
         [HttpGet]
         [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status200OK)]
         public IActionResult GetAll()
         {
+            var users = _context.Users.ToList();
+            var usersDto = _mapper.Map<List<UserResponseDTO>>(users);
             var response = new ApiResponse()
             {
                 IsSuccess = true,
-                Result = _context.Users.ToList(),
+                Result = usersDto,
                 StatusCode = HttpStatusCode.OK
             };
             return Ok(response);
@@ -43,8 +46,9 @@ namespace CryptoWalletApi.Controllers
         public IActionResult Get(int id)
         {
             var user = _context.Users.FirstOrDefault(u => u.Id == id);
+            var userDto = _mapper.Map<UserResponseDTO>(user);
             var response = new ApiResponse();
-            if (user is null)
+            if (userDto is null)
             {
                 response.IsSuccess = false;
                 response.StatusCode = HttpStatusCode.NotFound;
@@ -53,7 +57,7 @@ namespace CryptoWalletApi.Controllers
             }
 
             response.IsSuccess = true;
-            response.Result = user;
+            response.Result = userDto;
             response.StatusCode = HttpStatusCode.OK;
             return Ok(response);
         }
@@ -62,80 +66,71 @@ namespace CryptoWalletApi.Controllers
         [HttpPost]
         [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status201Created)]
         [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status400BadRequest)]
-        public IActionResult Create([FromBody] string name, string email, string password)
+        public IActionResult Create([FromBody] UserCreateDTO request)
         {
             var response = new ApiResponse()
             {
                 IsSuccess = false,
                 StatusCode = HttpStatusCode.BadRequest
             };
-
-            if (name is null || email is null || password is null)
+            if (!ModelState.IsValid)
             {
                 return BadRequest(response);
             }
 
-            var HighId = _context.Users.Max(u => (int?)u.Id);
-
-            var user = new User
-            {
-                Id = HighId == null ? 1 : HighId.Value + 1,
-                Name = name,
-                Email = email,
-                Wallet = null,
-                PasswordHash = password // will be changed
-
-            };
+            var user = _mapper.Map<User>(request);
             _context.Users.Add(user);
-
-            response.IsSuccess = true;
-            response.Result = user;
-            response.StatusCode = HttpStatusCode.Created;
-
             _context.SaveChanges();
 
-            return Created($"/api/friends/{user.Name}", response);
+            var userDto = _mapper.Map<UserResponseDTO>(user);
+            response.Result = userDto;
+            response.IsSuccess = true;
+            response.StatusCode = HttpStatusCode.Created;
+
+            return CreatedAtAction(nameof(Get), new { id = user.Id }, response);
         }
 
 
         [HttpPut]
         [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status200OK)]
         [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
-        public IActionResult Edit([FromBody] int id, string? name, string? email, string? password)
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status400BadRequest)]
+        public IActionResult Edit([FromBody] UserUpdateDTO request)
         {
-            var response = new ApiResponse()
-            {
-                IsSuccess = false,
-                StatusCode = HttpStatusCode.NotFound
-            };
+            var response = new ApiResponse();
 
-            var user = _context.Users.Find(id);
-
-            if (user is null) // ✅ Verifica se o usuário existe
+            if (!ModelState.IsValid)
             {
-                return NotFound();
+
+
+                response.IsSuccess = false;
+                response.StatusCode = HttpStatusCode.BadRequest;
+                response.ErrorMessages = ["Dados inválidos."]
+                ;
+
+                return BadRequest(response);
             }
 
+            var user = _context.Users.Find(request.Id);
 
-            if (name != null)
+            if (user == null)
             {
-                user.Name = name;
-            }
-            if (email != null)
-            {
-                user.Email = email;
-            }
-            if (password != null)
-            {
-                user.PasswordHash = password;
+                response.IsSuccess = false;
+                response.StatusCode = HttpStatusCode.NotFound;
+                response.ErrorMessages = ["Usuário não encontrado."];
+                return NotFound(response);
             }
 
-            response.IsSuccess = true;
-            response.Result = user;
-            response.StatusCode = HttpStatusCode.OK;
+            // request => user
+            _mapper.Map(request, user);
 
             _context.SaveChanges();
 
+            var userDto = _mapper.Map<UserResponseDTO>(user);
+
+            response.IsSuccess = true;
+            response.StatusCode = HttpStatusCode.OK;
+            response.Result = userDto;
             return Ok(response);
         }
 
