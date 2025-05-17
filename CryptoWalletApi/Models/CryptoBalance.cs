cc65ce4c9cf0bd2ff63930bca5cedb2bb0d50c6a
Lucas-AVS <lucas.alencar31@gmail.com>
2025-05-16 22:06:02 -0300

    chore

--- CryptoWalletApi/Models/CryptoBalance.cs
+++ CryptoWalletApi/Models/CryptoBalance.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,10 +9,8 @@ namespace CryptoWalletApi.Models
     public class CryptoBalance
     {
         public int Id { get; set; }
-
         public int WalletId { get; set; }
         public Wallet Wallet { get; set; }
-
         public string Currency { get; set; } // e.g. "BTC", "ETH"
         public decimal Amount { get; set; }
     }
--- CryptoWalletApi/Models/Transaction.cs
+++ CryptoWalletApi/Models/Transaction.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,15 +9,12 @@ namespace CryptoWalletApi.Models
     public class Transaction
     {
         public int Id { get; set; }
-
         public int SenderWalletId { get; set; }
         public Wallet SenderWallet { get; set; }
-
         public int ReceiverWalletId { get; set; }
         public Wallet ReceiverWallet { get; set; }
-
         public string Currency { get; set; } // e.g. "BTC", "ETH"
         public decimal Amount { get; set; }
-        public DateTime Timestamp { get; set; }
+        public DateTime Timestamp { get; set; } = DateTime.UtcNow;
     }
 }
