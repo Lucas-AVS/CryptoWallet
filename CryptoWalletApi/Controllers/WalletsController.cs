550ea4cb0851cf7997e1d42005e2079801792559
Lucas-AVS <lucas.alencar31@gmail.com>
2025-05-24 17:02:49 -0300

    feat: add WalletsController(get)

---
+++ CryptoWalletApi/Controllers/WalletsController.cs
@@ -0,0 +1,77 @@
+
+using System.Net;
+using AutoMapper;
+using CryptoWalletApi.Data;
+using CryptoWalletApi.DTO;
+using CryptoWalletApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CryptoWalletApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WalletsController : ControllerBase
+    {
+        private readonly ApiContext _context;
+        private readonly IMapper _mapper;
+
+        private readonly ILogger<WalletsController> _logger;
+
+        public WalletsController(ApiContext context, IMapper mapper, ILogger<WalletsController> logger)
+        {
+            _context = context;
+            _mapper = mapper;
+            _logger = logger;
+        }
+
+
+
+        [HttpGet]
+        [ProducesResponseType(typeof(ApiResponse<List<WalletDTO>>), StatusCodes.Status200OK)]
+        public IActionResult GetAll()
+        {
+            var wallets = _context.Wallets.Include(w => w.CryptoBalances).ToList();
+            var walletsDto = _mapper.Map<List<WalletDTO>>(wallets);
+
+            var response = new ApiResponse<List<WalletDTO>>()
+            {
+                IsSuccess = true,
+                Result = walletsDto,
+                StatusCode = HttpStatusCode.OK
+            };
+
+            return Ok(response);
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(typeof(ApiResponse<WalletDTO>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ApiResponse<WalletDTO>), StatusCodes.Status404NotFound)]
+        public IActionResult Get(int id)
+        {
+            var wallet = _context.Wallets
+                .Include(w => w.CryptoBalances)
+                .FirstOrDefault(w => w.Id == id);
+
+            if (wallet is null)
+            {
+                return NotFound(new ApiResponse<WalletDTO>
+                {
+                    IsSuccess = false,
+                    StatusCode = HttpStatusCode.NotFound,
+                    ErrorMessages = new List<string> { $"Wallet {id} not found" }
+                });
+            }
+
+            var walletDto = _mapper.Map<WalletDTO>(wallet);
+
+            return Ok(new ApiResponse<WalletDTO>
+            {
+                IsSuccess = true,
+                Result = walletDto,
+                StatusCode = HttpStatusCode.OK
+            });
+        }
+
+    }
+}
