4876ec0f2f65684b20a17386e32265c6694b7303
Lucas-AVS <lucas.alencar31@gmail.com>
2025-05-27 19:34:24 -0300

    feat: add initial database migration

---
+++ CryptoWalletApi/Migrations/20250527214135_InitialCreate.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace CryptoWalletApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "user",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "text", nullable: false),
+                    email = table.Column<string>(type: "text", nullable: false),
+                    password = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_user", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "wallet",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    user_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_wallet", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_wallet_user_user_id",
+                        column: x => x.user_id,
+                        principalTable: "user",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "crypto_balance",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    wallet_id = table.Column<int>(type: "integer", nullable: false),
+                    Currency = table.Column<string>(type: "text", nullable: false),
+                    Amount = table.Column<decimal>(type: "numeric", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_crypto_balance", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_crypto_balance_wallet_wallet_id",
+                        column: x => x.wallet_id,
+                        principalTable: "wallet",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "transaction",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    sender_wallet_id = table.Column<int>(type: "integer", nullable: false),
+                    receiver_wallet_id = table.Column<int>(type: "integer", nullable: false),
+                    currency = table.Column<string>(type: "text", nullable: false),
+                    amount = table.Column<decimal>(type: "numeric", nullable: false),
+                    timestamp = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_transaction", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_transaction_wallet_receiver_wallet_id",
+                        column: x => x.receiver_wallet_id,
+                        principalTable: "wallet",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_transaction_wallet_sender_wallet_id",
+                        column: x => x.sender_wallet_id,
+                        principalTable: "wallet",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_crypto_balance_wallet_id_Currency",
+                table: "crypto_balance",
+                columns: new[] { "wallet_id", "Currency" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_transaction_receiver_wallet_id",
+                table: "transaction",
+                column: "receiver_wallet_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_transaction_sender_wallet_id",
+                table: "transaction",
+                column: "sender_wallet_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_user_email",
+                table: "user",
+                column: "email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_wallet_user_id",
+                table: "wallet",
+                column: "user_id",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "crypto_balance");
+
+            migrationBuilder.DropTable(
+                name: "transaction");
+
+            migrationBuilder.DropTable(
+                name: "wallet");
+
+            migrationBuilder.DropTable(
+                name: "user");
+        }
+    }
+}
