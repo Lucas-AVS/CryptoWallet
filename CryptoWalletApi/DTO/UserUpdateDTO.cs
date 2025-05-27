b5700df14bd53bef329c0d458af80fbc57199971
Lucas-AVS <lucas.alencar31@gmail.com>
2025-05-27 19:32:14 -0300

    refactor: use email as user identifier instead of ID for user updates

--- CryptoWalletApi/DTO/UserUpdateDTO.cs
+++ CryptoWalletApi/DTO/UserUpdateDTO.cs
@@ -8,14 +8,12 @@ namespace CryptoWalletApi.DTO
 {
     public class UserUpdateDTO
     {
-        [Required]
-        public int Id { get; set; }
-
-        [MinLength(2)]
-        public string Name { get; set; }
 
         [EmailAddress]
-        public string? Email { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [MinLength(2)]
+        public string? Name { get; set; }
 
         [MinLength(6)]
         public string? NewPassword { get; set; }
